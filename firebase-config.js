9623f052599da06cd77ff4dfa965295c1904c252
maledannirh-design <male.dannirh@gmail.com>
2026-06-21 20:29:00 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAI2Xn2CbfE9m78n8uDGzccr9mron0scaI",
+  authDomain: "giltianappsmobile.firebaseapp.com",
+  databaseURL: "https://giltianappsmobile-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "giltianappsmobile",
+  storageBucket: "giltianappsmobile.firebasestorage.app",
+  messagingSenderId: "611921553199",
+  appId: "1:611921553199:web:319a304afc1f1e903e63da"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
